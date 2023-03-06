d39a0a005caf02c54a4e37c5125b9a80810b39cb
Grzegorz Wojtanowicz <g.wojtanowicz@zoovu.com>
2023-03-06 17:32:34 +0100

    get and write todos from db

--- igregor_todo/database.py
+++ igregor_todo/database.py
@@ -1,9 +1,11 @@
 """Database"""
 
 import configparser
+import json
 from pathlib import Path
+from typing import Any, Dict, List, NamedTuple
 
-from igregor_todo import DB_WRITE_ERROR, SUCCESS
+from igregor_todo import DB_WRITE_ERROR, SUCCESS, DB_READ_ERROR, JSON_ERROR
 
 DEFAULT_DB_FILE_PATH = Path.home().joinpath(
     "." + Path.home().stem + "_todo.json"
@@ -24,3 +26,29 @@ def init_database(db_path: Path) -> int:
         return SUCCESS
     except OSError:
         return DB_WRITE_ERROR
+
+class DBResponse(NamedTuple):
+    todo_list: List[Dict[str, Any]]
+    error: int
+
+class DatabaseHandler:
+    def __init__(self, db_path: Path) -> None:
+        self.__db_path__ = db_path
+
+    def read_todos(self) -> DBResponse:
+        try:
+            with self.__db_path__.open("r") as db:
+                try:
+                    return DBResponse(json.load(db), SUCCESS)
+                except json.JSONDecodeError: # Catch wrong JSON format
+                    return DBResponse([], JSON_ERROR)
+        except OSError: # Catch file IO problems
+            return DBResponse([], DB_READ_ERROR)
+    
+    def write_todos(self, todo_list: List[Dict[str, Any]]) -> DBResponse:
+        try: 
+            with self.__db_path__.open("w") as db:
+                json.dump(todo_list, db, indent=4)
+            return DBResponse(todo_list, SUCCESS)
+        except OSError: # Catch file IO problems
+            return DBResponse([], DB_WRITE_ERROR)
---
+++ igregor_todo/igregor_todo.py
@@ -0,0 +1,7 @@
+"""This module provides the RP To-Do model-controller."""
+
+from typing import Any, Dict, NamedTuple
+
+class CurrentTodo(NamedTuple):
+    todo: Dict[str, Any]
+    error: int
